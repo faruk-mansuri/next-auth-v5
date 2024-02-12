7f603dd75d8c91cd4238d31ce9814911c4db7ec2
faruq <faruqmansuri707@gmail.com>
2024-02-12 16:04:35 +0530

    fix client page bug

--- app/(protected)/client/page.js
+++ app/(protected)/client/page.js
@@ -1,10 +1,19 @@
 'use client';
 import UserInfo from '@/components/UserInfo';
 import { useCurrentUser } from '@/hooks/useCurrentUser';
+import { useEffect, useState } from 'react';
 
 const ClientPage = () => {
   const user = useCurrentUser();
-  return <UserInfo user={user} label='📱 Client Component' />;
+  const [currentUser, setCurrentUser] = useState(user);
+
+  useEffect(() => {
+    if (user) {
+      setCurrentUser(user);
+    }
+  }, [user]);
+
+  return <UserInfo user={currentUser} label='📱 Client Component' />;
 };
 
 export default ClientPage;
